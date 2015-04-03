9bc1cb40618b345ad30b1b14117d5f151f0e14ed
niiyz <yoshida@niiyz.com>
2015-04-04 01:15:18 +0900

    画像ファイル名取得。

---
+++ get_image_list.php
@@ -0,0 +1,43 @@
+<?php
+
+class GetImageList
+{    
+    public function __construct()
+    {
+    }
+    
+    public function load()
+    {
+        $photoList = $this->getPathList("./images/photo");
+        $iconList = $this->getPathList("./images/icon");
+        $result = ["photo" => $photoList, "pin" => $iconList];
+        echo json_encode($result);
+    }
+    
+    /**
+     * getPathList
+     * @param string $dir
+     * @return array $list
+     */
+    protected function getPathList($dir)
+    {
+        $iterator = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator(
+                $dir,
+                \FilesystemIterator::SKIP_DOTS
+                | \FilesystemIterator::KEY_AS_PATHNAME
+                | \FilesystemIterator::CURRENT_AS_FILEINFO
+            ), \RecursiveIteratorIterator::SELF_FIRST
+        );
+        $list = [];
+        foreach ($iterator as $pathName => $info) {
+            if ($info->isFile()) {
+                $list[] = $info->getFilename();
+            }
+        }
+        return $list;
+    }
+}
+
+$img = new GetImageList;
+$img->load();
